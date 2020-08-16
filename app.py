b0637c84d6bac1b884c76574929e4bc4fbbb8336
SUJAY S <44267241+sujay12345@users.noreply.github.com>
2020-08-16 18:38:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+filename = 'linearregressionmodel.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        if request.method == 'POST':
+            grescore = int(request.form['gre'])
+            toefl = int(request.form['toefl'])
+            urating = int(request.form['Universityrating'])
+            sop = int(request.form['sop'])
+            lor = int(request.form['lor'])
+            gpa = float(request.form['gpa'])
+            re = float(request.form['re'])   
+            return render_template('results.html',prediction=('{}%'.format(round(model.predict([[grescore,toefl,urating,sop,lor,gpa,re]])[0]*100, 1))))
+    except ValueError:
+        return render_template('index.html', message=" OOPs! Bad credentials. Enter valid numbers and Try again.")
+        
+if __name__ == '__main__':
+	app.run()
